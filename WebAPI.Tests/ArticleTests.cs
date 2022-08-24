35e05c9e77d04df7ac8f1a54893d0859e36602a0
MATEBOOK\piotr <piotr.ogorzelski@wp.pl>
2022-08-24 09:48:03 +0200

    .

---
+++ WebAPI.Tests/ArticleTests.cs
@@ -0,0 +1,41 @@
+﻿using Moq;
+using NUnit.Framework;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using WebAPI.Application.Dto;
+using WebAPI.Application.Interfaces;
+using WebAPI.Controllers;
+
+namespace WebAPI.Tests
+{
+    public class ArticleTests
+    {
+        [SetUp]
+        public void Setup()
+        {
+        }
+
+        [Test]
+        public void Test1()
+        {
+            // Arrange
+            var mockRepository = new Mock<IArticleService>();
+            mockRepository.Setup(x => x.GetArticle(42))
+                .Returns(new ArticleResponseDto { Id = 42 });
+
+            var controller = new ArticleController(mockRepository.Object);
+
+            // Act
+            IHttpActionResult actionResult = controller.Get(42);
+            var contentResult = actionResult as OkNegotiatedContentResult<Product>;
+
+            // Assert
+            Assert.IsNotNull(contentResult);
+            Assert.IsNotNull(contentResult.Content);
+            Assert.AreEqual(42, contentResult.Content.Id);
+        }
+    }
+}
